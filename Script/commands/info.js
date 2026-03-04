21031711aaee7b00ae853f164ae18b5702bc063a
ejhshs727 <rahatbbz58@gmail.com>
2026-03-04 18:37:45 +0600

    Update info.js

--- Script/commands/info.js
+++ Script/commands/info.js
@@ -22,23 +22,23 @@ module.exports.run = async function({ api, event }) {
 ┏━━━━━━━━━━━━━━━━┓
 ┃  🌟𝐎𝐖𝐍𝐄𝐑 𝐈𝐍𝐅𝐎🌟
 ┣━━━━━━━━━━━━━━━━┫
-┃👤𝐍𝐀𝐌𝐄      :𝐑𝐀𝐇𝐀𝐓
+┃👤𝐍𝐀𝐌𝐄      :𝐑𝐀𝐇𝐀𝐓 𝐁𝐁𝐙🤍
 ┃🚹𝐆𝐄𝐍𝐃𝐄𝐑    :𝐌𝐀𝐋𝐄
-┃🎂𝐀𝐆𝐄       :𝟏𝟔
+┃🎂𝐀𝐆𝐄       :18
 ┃🕌𝐑𝐄𝐋𝐈𝐆𝐈𝐎𝐍 : 𝐈𝐒𝐋𝐀𝐌
-┃🏫𝐄𝐃𝐔𝐂𝐀𝐓𝐈𝐎𝐍 :বয়ড়া ইসরাইল
-┃🏡𝐀𝐃𝐃𝐑𝐄𝐒𝐒 :জামালপুর,বাংলাদেশ
+┃🏫𝐄𝐃𝐔𝐂𝐀𝐓𝐈𝐎𝐍 :𝐌𝐘𝐌𝐄𝐍𝐒𝐈𝐍𝐆𝐇 𝐋𝐎𝐂𝐀𝐋 𝐂𝐎𝐋𝐋𝐄𝐆𝐄 
+┃🏡𝐀𝐃𝐃𝐑𝐄𝐒𝐒 :𝐌𝐘𝐌𝐄𝐍𝐒𝐈𝐍𝐆𝐇 𝐂𝐈𝐓𝐘
 ┣━━━━━━━━━━━━━━━━┫
-┃𝐓𝐈𝐊𝐓𝐎𝐊 :@where.is.she15
-┃📢𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌 :দিবো না🥴🤪
-┃🌐𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 :বায়ো-তে আছে
+┃𝐓𝐈𝐊𝐓𝐎𝐊 :@𝐫𝐚𝐡𝐚𝐭 𝐟𝐟 71🤍
+┃📢𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌 :𝐑𝐀𝐇𝐀𝐓 𝐁𝐀𝐁𝐘👄
+┃🌐𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 :𝐀𝐁𝐈𝐑 𝐇𝐀𝐒𝐀𝐍 𝐑𝐀𝐇𝐀𝐓
 ┣━━━━━━━━━━━━━━━━┫
 ┃🕒𝐔𝐏𝐃𝐀𝐓𝐄𝐃 𝐓𝐈𝐌𝐄 :${time}
 ┗━━━━━━━━━━━━━━━━┛`,
  attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.jpg"));
 
- return request("https://i.imgur.com/FJI61jS.jpeg")
+ return request("https://i.imgur.com/6WUSg9u.jpeg")
  .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
  .on('close', () => callback());
 };
