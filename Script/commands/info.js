6afeb90ed39386798c3a0724efccab39f98cec8c
ejhshs727 <rahatbbz58@gmail.com>
2026-03-09 04:57:40 +0600

    Update info.js

--- Script/commands/info.js
+++ Script/commands/info.js
@@ -22,23 +22,23 @@ module.exports.run = async function({ api, event }) {
 ┏━━━━━━━━━━━━━━━━┓
 ┃  🌟𝐎𝐖𝐍𝐄𝐑 𝐈𝐍𝐅𝐎🌟
 ┣━━━━━━━━━━━━━━━━┫
-┃👤𝐍𝐀𝐌𝐄      :Nasrin Chowdhury Riya🤍
-┃🚹𝐆𝐄𝐍𝐃𝐄𝐑    :Female 
+┃👤𝐍𝐀𝐌𝐄      :𝐑𝐀𝐇𝐔 𝐁𝐁𝐙🤍
+┃🚹𝐆𝐄𝐍𝐃𝐄𝐑    :𝐌𝐀𝐋𝐄
 ┃🎂𝐀𝐆𝐄       :18+
 ┃🕌𝐑𝐄𝐋𝐈𝐆𝐈𝐎𝐍 : 𝐈𝐒𝐋𝐀𝐌
-┃🏫𝐄𝐃𝐔𝐂𝐀𝐓𝐈𝐎𝐍 :vondami 𝐋𝐎𝐂𝐀𝐋 𝐂𝐎𝐋𝐋𝐄𝐆𝐄 
-┃🏡𝐀𝐃𝐃𝐑𝐄𝐒𝐒 :Chittagong 𝐂𝐈𝐓𝐘
+┃🏫𝐄𝐃𝐔𝐂𝐀𝐓𝐈𝐎𝐍 :𝐌𝐘𝐌𝐄𝐍𝐒𝐈𝐍𝐆𝐇 𝐋𝐎𝐂𝐀𝐋 𝐂𝐎𝐋𝐋𝐄𝐆𝐄 
+┃🏡𝐀𝐃𝐃𝐑𝐄𝐒𝐒 :𝐌𝐘𝐌𝐄𝐍𝐒𝐈𝐍𝐆𝐇 𝐂𝐈𝐓𝐘
 ┣━━━━━━━━━━━━━━━━┫
-┃𝐓𝐈𝐊𝐓𝐎𝐊 :@Ogmusic🎵admin
-┃📢𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌 :NAI
-┃🌐𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 :Nasrin Chowdhury Riya
+┃𝐓𝐈𝐊𝐓𝐎𝐊 :@𝐲𝐨𝐮𝐫 𝐫𝐚𝐡𝐮 𝐛𝐛𝐳💚
+┃📢𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌 :𝐑𝐀𝐇𝐀𝐓 𝐁𝐀𝐁𝐘🦅
+┃🌐𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 :𝐀𝐁𝐈𝐑 𝐇𝐀𝐒𝐀𝐍 𝐑𝐀𝐇𝐀𝐓
 ┣━━━━━━━━━━━━━━━━┫
 ┃🕒𝐔𝐏𝐃𝐀𝐓𝐄𝐃 𝐓𝐈𝐌𝐄 :${time}
 ┗━━━━━━━━━━━━━━━━┛`,
  attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.jpg"));
 
- return request("https://i.imgur.com/HKdBp9X.jpeg")
+ return request("https://i.imgur.com/o5JPIBz.jpeg")
  .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
  .on('close', () => callback());
 };
